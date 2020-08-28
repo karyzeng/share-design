bfef1323df21c12e14694c327790eafde817d3cb
karyzeng <354697171@qq.com>
2020-08-28 17:36:01 +0800

    YTAD GPS信息处理handler

---
+++ zzp.design.gps/src/main/java/com/zzp/gps/handler/YtadGpsHandler.java
@@ -0,0 +1,19 @@
+package com.zzp.gps.handler;
+
+import com.zzp.gps.domain.GpsApiConfig;
+import com.zzp.gps.domain.Vehicle;
+
+import java.util.List;
+
+/**
+ * @Description YTAD GPS信息处理handler
+ * @Author karyzeng
+ * @since 2020.08.28
+ **/
+public class YtadGpsHandler extends BaseGpsHandler{
+
+    @Override
+    public List<Vehicle> internalListGpsInfos(String vehicles, GpsApiConfig config) {
+        return null;
+    }
+}
