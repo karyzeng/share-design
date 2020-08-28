c8bba6f1bb1da3d787d0ee4f79f656b4d0397335
karyzeng <354697171@qq.com>
2020-08-28 17:35:48 +0800

    YL GPS信息处理handler

---
+++ zzp.design.gps/src/main/java/com/zzp/gps/handler/YlGpsHandler.java
@@ -0,0 +1,19 @@
+package com.zzp.gps.handler;
+
+import com.zzp.gps.domain.GpsApiConfig;
+import com.zzp.gps.domain.Vehicle;
+
+import java.util.List;
+
+/**
+ * @Description YL GPS信息处理handler
+ * @Author karyzeng
+ * @since 2020.08.28
+ **/
+public class YlGpsHandler extends BaseGpsHandler{
+
+    @Override
+    public List<Vehicle> internalListGpsInfos(String vehicles, GpsApiConfig config) {
+        return null;
+    }
+}
