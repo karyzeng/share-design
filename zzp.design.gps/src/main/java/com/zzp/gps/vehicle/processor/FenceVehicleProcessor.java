6e7e92f8bd2f587e35c551a93ebbdeb2b5d76a4f
karyzeng <354697171@qq.com>
2020-08-28 17:38:08 +0800

    有电子围栏的车辆Processor

---
+++ zzp.design.gps/src/main/java/com/zzp/gps/vehicle/processor/FenceVehicleProcessor.java
@@ -0,0 +1,14 @@
+package com.zzp.gps.vehicle.processor;
+
+/**
+ * @Description 有电子围栏的车辆Processor
+ * @Author karyzeng
+ * @since 2020.08.28
+ **/
+public class FenceVehicleProcessor extends BaseVehicleProcessor{
+
+    @Override
+    public void doRun() {
+
+    }
+}
