86da7a26ed3e5f9e37f0b54fabfa6164bd317aca
karyzeng <354697171@qq.com>
2020-09-03 17:58:54 +0800

    公共商品Vo

---
+++ zzp.design.schedule.task/src/main/java/com/zzp/scheduled/task/vo/CommonGoods.java
@@ -0,0 +1,36 @@
+package com.zzp.scheduled.task.vo;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * @Description 公共商品
+ * @Author Garyzeng
+ * @since 2020.09.03
+ **/
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+public class CommonGoods implements Serializable {
+
+    private static final long serialVersionUID = -1L;
+
+    /**
+     * id
+     */
+    private Integer id;
+
+    /**
+     * 商品名称
+     */
+    private String name;
+
+    /**
+     * 商品数量
+     */
+    private Integer size;
+
+}
