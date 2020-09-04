06dc43493385376b43b86b8afa2ed476820b37b5
karyzeng <354697171@qq.com>
2020-09-04 17:42:14 +0800

    修改公共商品A类适配器

--- zzp.design.schedule.task/src/main/java/com/zzp/scheduled/task/listeners/CommonGoodsAAdapter.java
+++ zzp.design.schedule.task/src/main/java/com/zzp/scheduled/task/listeners/CommonGoodsAAdapter.java
@@ -1,6 +1,7 @@
 package com.zzp.scheduled.task.listeners;
 
 import com.zzp.scheduled.task.vo.CommonGoods;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
@@ -9,15 +10,17 @@ import java.util.List;
  * @Author karyzeng
  * @since 2020.09.03
  **/
+@Component(value = "commonGoodsAAdapter")
 public class CommonGoodsAAdapter extends BaseTaskListener<CommonGoods>{
 
     @Override
-    public void internalSave(List<CommonGoods> list) {
-
+    public void save(List<CommonGoods> list) {
+        System.out.println("调用保存公共商品方法");
     }
 
     @Override
-    public List<CommonGoods> internalCount() {
+    public List<CommonGoods> count() {
+        System.out.println("调用汇总公共商品A方法");
         return null;
     }
 }
