9e6150a98b89ed390895d36fe3e82bb8ed8b90a6
karyzeng <354697171@qq.com>
2020-09-04 17:41:54 +0800

    修改定时子任务基类

--- zzp.design.schedule.task/src/main/java/com/zzp/scheduled/task/listeners/BaseTaskListener.java
+++ zzp.design.schedule.task/src/main/java/com/zzp/scheduled/task/listeners/BaseTaskListener.java
@@ -1,31 +1,20 @@
 package com.zzp.scheduled.task.listeners;
 
-import org.apache.poi.ss.formula.functions.T;
-
 import java.util.List;
 
 /**
  * @Description 定时子任务基类
- * @Author Garyzeng
+ * @Author karyzeng
  * @since 2020.09.03
  **/
-public abstract class BaseTaskListener<T> implements TaskListener<T>{
+public abstract class BaseTaskListener<T> implements TaskListener{
 
     @Override
-    public void save(List<T> list) {
-        this.internalSave(list);
-    }
-
-    @Override
-    public List<T> count() {
-        return this.internalCount();
-    }
-
     public void countAndSave() {
         this.save(this.count());
     }
 
-    public abstract void internalSave(List<T> list);
+    public abstract void save(List<T> list);
 
-    public abstract List<T> internalCount();
+    public abstract List<T> count();
 }
