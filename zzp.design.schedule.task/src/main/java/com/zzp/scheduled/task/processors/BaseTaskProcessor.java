907042f88bbd12e6844559f9489170956347b408
karyzeng <354697171@qq.com>
2020-09-04 17:43:00 +0800

    定时任务Processor基类

---
+++ zzp.design.schedule.task/src/main/java/com/zzp/scheduled/task/processors/BaseTaskProcessor.java
@@ -0,0 +1,43 @@
+package com.zzp.scheduled.task.processors;
+
+import com.zzp.scheduled.task.listeners.TaskListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Description 定时任务Processor基类
+ * @Author karyzeng
+ * @since 2020.09.04
+ **/
+public abstract class BaseTaskProcessor {
+
+    private List<TaskListener> listeners;
+
+    public BaseTaskProcessor() {
+        listeners = new ArrayList<TaskListener>();
+    }
+
+    public void addListener(TaskListener listener) {
+        if (listener != null) {
+            listeners.add(listener);
+        }
+    }
+
+    public abstract void beforeRun();
+
+    public abstract void afterRun();
+
+    public abstract void start();
+
+    public void run() {
+        this.beforeRun();
+
+        for (TaskListener listener : listeners) {
+            listener.countAndSave();
+        }
+
+        this.afterRun();
+    }
+
+}
